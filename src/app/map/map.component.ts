acfed8c3186cfe89a82b57b650342f058fcbc852
KameliyaKK <93966328+KameliyaKK@users.noreply.github.com>
2022-04-02 15:05:31 +0300

    Map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -4,16 +4,58 @@ import { DataService } from "../data.service";
 
 
 @Component({
-  selector: 'app-map',
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+	selector: 'app-map',
+	templateUrl: './map.component.html',
+	styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
 
 
-  constructor() { }
+	constructor(
+		private dataService: DataService
+	) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
+
+	onMapReady(event: YaReadyEvent<ymaps.Map>): void {
+		const objectManagerOptions: ymaps.IObjectManagerOptions = {
+			clusterize: true,
+			gridSize: 30,
+			clusterDisableClickZoom: false,
+		};
+		const objectManager = new ymaps.ObjectManager(objectManagerOptions);
+		objectManager.clusters.options.set('preset', 'islands#yellowClusterIcons');
+		event.target.geoObjects.add(objectManager);
+
+		this.dataService.getData().subscribe((data: any) => {
+			data.forEach((element: any) => {
+				console.log(element);
+				objectManager.add({
+					type: 'Feature',
+					geometry: {
+						type: 'Point',
+						coordinates: [point.lat, point.lon],
+					},
+					options: {
+						"iconLayout": 'default#imageWithContent',
+						"iconImageHref": 'assets/img/map_icon_block2.svg',
+						"iconImageSize": [30, 42],
+						"iconImageOffset": [-3, -42],
+					},
+					properties: {
+						data
+					},
+
+				});
+			});
+
+		});
+
+
+
+
+
+	}
 
 }
